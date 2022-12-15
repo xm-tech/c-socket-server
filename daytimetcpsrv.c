0c2a5c1c4519f0a5f0287c64265c97c09ffe142d
xm-tech <maxm09@163.com>
2022-12-16 05:09:02 +0800

    test print

--- daytimetcpsrv.c
+++ daytimetcpsrv.c
@@ -14,19 +14,19 @@
 
 #define BACKLOG 100 /* sizeof(synrcved list) + sizeof(established list)*/
 #define SA struct sockaddr
+#define LISTEN_PORT 8888
 
 int main(int argc, char **argv) {
-	int listenfd, connfd;
 	socklen_t len;
 	struct sockaddr_in serveraddr, cliaddr;
 	char buff[1024];
 	time_t ticks;
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons(8888);
+	serveraddr.sin_port = htons(LISTEN_PORT);
 
 	bool binded = bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr));
 	printf("binded:%d\n", binded);
@@ -35,9 +35,11 @@ int main(int argc, char **argv) {
 	
 	while (true) {
 		len = sizeof(cliaddr);
-		connfd = accept(listenfd, (SA *) &cliaddr, &len);
+		int connfd = accept(listenfd, (SA *) &cliaddr, &len);
 		/* connfd = accept(listenfd, NULL, NULL); */
-		printf("conn from %s, port: %d, connfd: %d", inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)), ntohs(cliaddr.sin_port), connfd);
+		const char* caddr = inet_ntop(AF_INET,  &cliaddr.sin_addr, buff, sizeof(buff));
+		int cport = ntohs(cliaddr.sin_port);
+		printf("conn from %s, port: %d, connfd: %d", caddr, cport, connfd);
 		ticks = time(NULL);
 		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
 		write(connfd, buff, strlen(buff));
